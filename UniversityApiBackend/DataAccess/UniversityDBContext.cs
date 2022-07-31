b96e5fd6bcf0dd695cf3f42fab5fa97722ea337a
Eliseo Falcon <eliseonop@gmail.com>
2022-07-31 07:12:02 -0500

    first commit

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options)
+            : base(options)
+        {
+        }
+        // TODO : Add DbSet (Tables of our Database)
+        public DbSet<User>? Users { get; set; }
+    }
+    
+}
